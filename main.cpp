7f5aadc5dd4b193cd4b1d1270ce6c73ae7808723
jannatulborsha <jannatulborsha17@gmail.com>
2018-05-01 11:36:38 -0700

    updated

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include "add.h"
+#include "sub.h"
 
 using namespace std;
 
 int main()
 {
+    int x, y, z;
+    x = 10;
+    y = 20;
+    z = add(x,y);
+    cout << z << endl;
+    z = sub(y, x);
+    cout << z << endl;
     return 0;
 }
